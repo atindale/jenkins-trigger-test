35ff8be55d09d4eaaf2a810b90f82cbcc3d84e9c
Alan Tindale <alan@flexiblesoftware.com.au>
2018-03-16 14:55:25 +1000

    Change script to run job

--- is_last_day_of_month.sh
+++ is_last_day_of_month.sh
@@ -4,7 +4,9 @@ TODAY=`/bin/date +%d`
 TOMORROW=`/bin/date +%d -d "1 day"`
  
 if [ $TOMORROW -lt $TODAY ]; then
-   exit 0
+    echo "Last day of the month. Running jobs."
+    curl -X POST http://atindale:991c410d6a2aa477f90edee941f7e960@jenkins:8080/job/Test2/build?token=abc123
+    exit 0
 fi
  
-exit 1
+echo "Nothing to run today"
