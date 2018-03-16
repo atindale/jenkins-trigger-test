f021c076a704e82fcb7ecb50767ec994238bbc00
Alan Tindale <alan@flexiblesoftware.com.au>
2018-03-16 15:04:54 +1000

    Add debug print

--- is_last_day_of_month.sh
+++ is_last_day_of_month.sh
@@ -3,7 +3,10 @@
 TODAY=`/bin/date +%d`
 #TOMORROW=`/bin/date +%d -d "1 day"`
 TOMORROW=`/bin/date +%d -d "2018/03/16"`
- 
+
+echo "TODAY=$TODAY"
+echo "TOMORROW=$TOMORROW"
+
 if [ $TOMORROW -lt $TODAY ]; then
     echo "Last day of the month. Running jobs."
     curl -X POST http://atindale:991c410d6a2aa477f90edee941f7e960@jenkins:8080/job/Test2/build?token=abc123
